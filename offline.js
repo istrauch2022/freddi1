0df467c51efdab6d51253ae76947238c32a89974
istrauch2022 <154003408+istrauch2022@users.noreply.github.com>
2024-12-26 09:28:36 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1735186492,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Спрайт-sheet0.png",
+		"images/ПлиточныйФон.png",
+		"images/охранник-sheet0.png",
+		"images/фредди-sheet0.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт4-sheet1.png",
+		"images/Спрайт5-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
